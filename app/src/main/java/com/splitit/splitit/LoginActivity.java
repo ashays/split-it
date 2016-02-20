515030a721f97dcf109dc3623ebd5ad4af70cb5c
Ashay Sheth <ashay.sheth@gmail.com>
2016-02-20 02:06:42 -0500

    added fb import to login activity

--- app/src/main/java/com/splitit/splitit/LoginActivity.java
+++ app/src/main/java/com/splitit/splitit/LoginActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.firebase.client.Firebase;
+import com.facebook.FacebookSdk;
 
 public class LoginActivity extends AppCompatActivity {
 
