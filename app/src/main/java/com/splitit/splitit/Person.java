bdff69b84ae159a6e6787d74dc5721fe9e0018d7
dhruvsagar <dhruv.sagar@gatech.edu>
2016-02-20 05:25:47 -0500

    Made Person class

--- app/src/main/java/com/splitit/splitit/Person.java
+++ app/src/main/java/com/splitit/splitit/Person.java
@@ -4,6 +4,29 @@ package com.splitit.splitit;
  * Created by ciyengar on 2/20/16.
  */
 public class Person {
+    private String firstName, lastName, id;
+    private ArrayList<Trip> trips;
 
+    public Person(firstName, lastName, id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
 
+    public void setName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public ArrayList<Trip> getTrips() {
+        return trips;
+    }
 }
