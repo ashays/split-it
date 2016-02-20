59ca456edfb7a4b316d3fcb1365cf41361f6911b
Ashay Sheth <ashay.sheth@gmail.com>
2016-02-20 04:48:30 -0500

    can go to trips page wo logging in

--- app/src/main/java/com/splitit/splitit/LoginActivity.java
+++ app/src/main/java/com/splitit/splitit/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.splitit.splitit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,6 +27,8 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent i = new Intent(LoginActivity.this, TripActivity.class);
+                startActivity(i);
             }
         });
     }
