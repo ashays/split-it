5bc7832b9cf963e2784803830e83e29e8bc5e1da
Crishna Iyengar <ciyengar3@gmail.com>
2016-02-20 19:36:27 -0500

    Login Feature
    swag

--- app/src/main/java/com/splitit/splitit/LoginActivity.java
+++ app/src/main/java/com/splitit/splitit/LoginActivity.java
@@ -1,33 +1,88 @@
 package com.splitit.splitit;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.firebase.client.AuthData;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
-import com.facebook.FacebookSdk;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.firebase.ui.auth.core.AuthProviderType;
+import com.firebase.ui.auth.core.FirebaseLoginBaseActivity;
+import com.firebase.ui.auth.core.FirebaseLoginError;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends FirebaseLoginBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Firebase.setAndroidContext(this);
-        Firebase firebaseRef = new Firebase("https://split-it.firebaseio.com/");
-        final Firebase tripsRef = firebaseRef.child("trips");
         FacebookSdk.sdkInitialize(getApplicationContext());
+        LoginManager.getInstance().logOut();
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                facebookLogin();
+            }
+        });
+    }
 
+    public void facebookLogin() {
+        showFirebaseLoginPrompt();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setEnabledAuthProvider(AuthProviderType.FACEBOOK);
+
+    }
+
+    @Override
+    public Firebase getFirebaseRef() {
+        Firebase.setAndroidContext(this);
+        return new Firebase("https://split-it.firebaseio.com/");
+    }
+
+    @Override
+    public void onFirebaseLoginProviderError(FirebaseLoginError firebaseError) {
+        Context context = getApplicationContext();
+        CharSequence text = "Login Failed";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    @Override
+    public void onFirebaseLoginUserError(FirebaseLoginError firebaseError) {
+        Context context = getApplicationContext();
+        CharSequence text = "Login Failed";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    @Override
+    public void onFirebaseLoggedIn(AuthData authData) {
+        //Intent i = new Intent(LoginActivity.this, TripActivity.class);
+        //startActivity(i);
+        /**
+        Firebase fb = getFirebaseRef();
+        final Firebase tripsRef = fb.child("trips");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,7 +95,6 @@ public class LoginActivity extends AppCompatActivity {
                     public void onDataChange(DataSnapshot snapshot) {
                         System.out.println("There are " + snapshot.getChildrenCount() + " trips");
                         for (DataSnapshot postSnapshot: snapshot.getChildren()) {
-                            System.out.println("hihi");
                             String tripName = (String) postSnapshot.child("name").getValue();
                             TripActivity.currentUser.addTrip(new Trip(tripName));
                         }
@@ -54,6 +108,14 @@ public class LoginActivity extends AppCompatActivity {
                 });
             }
         });
+         **/
+
+    }
+
+    @Override
+    public void onFirebaseLoggedOut() {
+        //Intent i = new Intent(LoginActivity.this, TripActivity.class);
+        //startActivity(i);
     }
 
 }
